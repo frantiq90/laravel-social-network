0113936c7d9b7f76e7d4fa3217fd0e149d8e6c00
frantiq90 <oscar.kosela@gmail.com>
2017-08-10 11:45:06 +0000

    some validation for signin/signup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,22 +5,33 @@
 @endsection
 
 @section('content')
+    @if (count($errors) > 0)
+        <div class="row">
+            <div class="col-md-6">
+                <ul>
+                    @foreach($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+    @endif
     <div class="row">
         <div class="col-md-6">
             <h3>Sing Up</h3>
             <form action="{{ route('signup') }}" method="post">
                 {{ csrf_field() }}
-                <div class="form-group">
+                <div class="form-group  {{ $errors->has('email') ? 'has-error' : '' }}">
                     <label for="email">Your E-mail</label>
-                    <input  class="form-control" type="text" name="email" id="email">
+                    <input  class="form-control" type="text" name="email" id="email" value="{{ Request::old('email') }}">
                 </div>
-                <div class="form-group">
+                <div class="form-group {{ $errors->has('first_name') ? 'has-error' : '' }}">
                     <label for="first_name">Your First Name</label>
-                    <input  class="form-control" type="text" name="first_name" id="first_name">
+                    <input  class="form-control" type="text" name="first_name" id="first_name" value="{{ Request::old('first_name') }}">
                 </div>
-                <div class="form-group">
+                <div class="form-group {{ $errors->has('password') ? 'has-error' : '' }}">
                     <label for="password">Your Password</label>
-                    <input  class="form-control" type="Password" name="password" id="password">
+                    <input  class="form-control" type="Password" name="password" id="password" value="{{ Request::old('password') }}">
                 </div>
                 <button type="submit" class="btn btn-primary">Register</button>
             </form>
@@ -30,13 +41,13 @@
             <h3>Sing In</h3>
             <form action="{{ route('signin') }}" method="post">
                 {{ csrf_field() }}
-                <div class="form-group">
+                <div class="form-group {{ $errors->has('email') ? 'has-error' : '' }}">
                     <label for="email">Your E-mail</label>
-                    <input  class="form-control" type="text" name="email" id="email">
+                    <input  class="form-control" type="text" name="email" id="email" value="{{ Request::old('email') }}">
                 </div>
-                <div class="form-group">
+                <div class="form-group {{ $errors->has('password') ? 'has-error' : '' }}">
                     <label for="password">Your Password</label>
-                    <input  class="form-control" type="Password" name="password" id="password">
+                    <input  class="form-control" type="Password" name="password" id="password" value="{{ Request::old('password') }}">
                 </div>
                 <button type="submit" class="btn btn-primary">Login</button>
             </form>
